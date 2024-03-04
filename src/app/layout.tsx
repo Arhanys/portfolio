118b687a5ff1261214d2f6124f2eff1840d1dfc4
Ethan Colinet <126577076+Arhanys@users.noreply.github.com>
2024-03-04 12:44:51 +0100

    -Added custom fonts from google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,16 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import {Playfair_Display, Archivo_Black} from "next/font/google"
+const playfair = Playfair_Display({
+  weight:'700',
+  subsets: ['latin'],
+  variable: '--font-playfair',
+});
+const archivo = Archivo_Black({
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-archivo',
+})
 
 export const metadata: Metadata = {
   title: "Ethan's Portfolio",
@@ -12,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="h-screen">{children}</body>
+      <body className={`${archivo.variable} ${playfair.variable} h-screen bg-grass-3`}>{children}</body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,45 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
+    theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+
+      fontFamily:{
+        'playfair' : "var(--font-playfair)",
+        'archivo' : "var(--font-archivo)",
       },
+
+      colors: {
+        special: "#FCE6A4",
+        grass: {
+          1:"#0E1512" ,
+          2:"#121B17" ,
+          3:"#132D21" ,
+          4:"#113B29" ,
+          5:"#25482D" ,
+          6:"#20573E" ,
+          7:"#28684A" ,
+          8:"#2F7C57" ,
+          9:"#30A46C" ,
+          10:"#33B074" ,
+          11:"#3DD68C" ,
+          12:"#B1F1CB" 
+        },
+        amber:{
+          1: "#16120C",
+          2: "#1D180F",
+          3: "#302008",
+          4: "#3F2700",
+          5: "#4D3000",
+          6: "#5C3D05",
+          7: "#714F19",
+          8: "#8F6424",
+          9: "#FFC53D",
+          10: "#FFD60A",
+          11: "#FFCA16",
+          12: "#FFE7B3",
+        },
+      }
     },
   },
   plugins: [],
