3d930269671dd421097d621ff8df824c30c96953
Ethan Colinet <126577076+Arhanys@users.noreply.github.com>
2024-10-18 20:01:31 +0200

    Worked on skill section and did some modification on bento section. Still work TBD on both

--- src/components/Bento.tsx
+++ src/components/Bento.tsx
@@ -1,76 +1,72 @@
-"use client"
+"use client";
 
-import {gsap} from "gsap"
-import { useGSAP } from "@gsap/react"
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 import { useState } from "react";
 import { ScrollTrigger } from "gsap/all";
 
 gsap.registerPlugin(ScrollTrigger);
 
-
 export default function Bento() {
-    /* Create a state for the timeline */
-    const [tl, setTl] = useState<any | null>(null);
+  /* Create a state for the timeline */
+  const [tl, setTl] = useState<any | null>(null);
 
-    /* Animation */
-    useGSAP(()=>{
-        const items: Record<string, HTMLElement | null> = {};
+  /* Animation */
+  useGSAP(() => {
+    const items: Record<string, HTMLElement | null> = {};
 
-        for (let i = 1; i<= 7; i++) {
-            items[`item${i}`] = document.querySelector(`[data-index="${i}"]`);
-        }
+    for (let i = 1; i <= 7; i++) {
+      items[`item${i}`] = document.querySelector(`[data-index="${i}"]`);
+    }
 
-        const tl = gsap.timeline({defaults:{duration:0.3 }});
-        setTl(tl);
+    const tl = gsap.timeline({ defaults: { duration: 0.3 } });
+    setTl(tl);
 
-        tl.from(items.item5, { y: 1000 }, 1);
-        tl.from(items.item2, { x: -2000 }, 1.8);
-        tl.from(items.item3, { y: 1200 }, 1.7);
-        tl.from(items.item7, { x: -2000 }, 2);
-        tl.from(items.item4, { y: 1000 }, 2.2);
-        tl.from(items.item6, { x: -2000 }, 2.3);
-        tl.from(items.item1, { y: -1000 }, 2.4);
-        
-        /* Trigger animation with a trigger point */
-        ScrollTrigger.create({
-            animation: tl, // Pass your timeline to the animation property
-            trigger: '#projects', // Specify the trigger element or scroll position
-            start: () => `top-=300 center`,
-            end: 'bottom center',
-            toggleActions:'play reverse play reverse',
-        });
+    tl.from(items.item5, { y: 1000 }, 1);
+    tl.from(items.item2, { x: -2000 }, 1.8);
+    tl.from(items.item3, { y: 1200 }, 1.7);
+    tl.from(items.item7, { x: -2000 }, 2);
+    tl.from(items.item4, { y: 1000 }, 2.2);
+    tl.from(items.item6, { x: -2000 }, 2.3);
+    tl.from(items.item1, { y: -1000 }, 2.4);
 
-    }, []);
+    /* Trigger animation with a trigger point */
+    ScrollTrigger.create({
+      animation: tl, // Pass your timeline to the animation property
+      trigger: "#projects", // Specify the trigger element or scroll position
+      start: () => `top-=300 center`,
+      end: "bottom center",
+      toggleActions: "play reverse play reverse",
+    });
+  }, []);
 
-    return(
-        /* Section */
-        <section id="projects" className="bg-grass-3 h-screen flex items-center justify-center overflow-hidden">
-            {/* Container of my moving divs */}
-            <div id="bentoContainer" className="grid grid-rows-4 grid-cols-6 bg-grass-1 h-4/5 w-4/5 rounded-xl p-6 gap-4">
-            <div data-index="1" id="bentoItem" className=" row-span-3 col-span-2">
-                <img src="img/LogoCharlynArt.png" alt=""/>
-                <h2 className="absolute bottom-2 2xl:bottom-8 font-playfair text-5xl 2xl:text-7xl text-[#CB1D63]">CharlynArt</h2>
-            </div>
-            <div data-index="2" id="bentoItem" className="col-span-3">
-                <h2 className="text-5xl 2xl:text-6xl font-archivo italic text-special">INSERT SOME TEXT</h2>
-            </div>
-            <div data-index="3" id="bentoItem" className=" row-span-4">
-                <h2 className="text-special font-archivo text-6xl 2xl:text-7xl whitespace-nowrap rotate-90">Future is now !</h2>
-            </div>
-            <div data-index="4" id="bentoItem" className=" row-span-3">
-                <h2 className="text-[2.7rem] 2xl:text-6xl justify-center font-archivo text-special -rotate-90  whitespace-nowrap">Fun and Design</h2>
-            </div>
-            <div data-index="5" id="bentoItem" className=" col-span-2 row-span-2 ">
-                <h2 className=" text-8xl 2xl:text-9xl font-archivo ">Zeta</h2>
-            </div>
-            <div data-index="6" id="bentoItem" className=" col-span-2">
-                <h2 className="text-7xl 2xl:text-8xl font-playfair text-amber-9">Portfolio</h2>
-            </div>
-            <div data-index="7" id="bentoItem" className=" col-span-2">
-                <h2 className="text-7xl 2xl:text-8xl font-playfair text-amber-9">Projects</h2>
-            </div>
-
-            </div>
-        </section>
-    )
+  return (
+    /* Section */
+    <section id="projects" className="bg-grass-3 h-screen flex items-center justify-center overflow-hidden">
+      {/* Container of my moving divs */}
+      <div id="bentoContainer" className="grid grid-rows-4 grid-cols-6 bg-grass-1 h-4/5 w-4/5 rounded-xl p-6 gap-4">
+        <div data-index="1" id="bentoItem" className=" row-span-3 col-span-2"></div>
+        <div data-index="2" id="bentoItem" className="col-span-3">
+          <h2 className="text-5xl 2xl:text-6xl font-archivo italic text-special">English speaker</h2>
+        </div>
+        <div data-index="3" id="bentoItem" className=" row-span-4">
+          <h2 className="text-special font-archivo text-6xl 2xl:text-5xl whitespace-nowrap rotate-90">
+            Ready for action
+          </h2>
+        </div>
+        <div data-index="4" id="bentoItem" className=" row-span-3">
+          <h2 className="text-[2.7rem] 2xl:text-6xl justify-center font-archivo text-special -rotate-90  whitespace-nowrap">
+            Design first
+          </h2>
+        </div>
+        <div data-index="5" id="bentoItem" className=" col-span-2 row-span-2 "></div>
+        <div data-index="6" id="bentoItem" className=" col-span-2">
+          <h2 className="text-7xl 2xl:text-8xl font-playfair text-amber-9">Portfolio</h2>
+        </div>
+        <div data-index="7" id="bentoItem" className=" col-span-2">
+          <h2 className="text-7xl 2xl:text-8xl font-playfair text-amber-9">Projects</h2>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,7 +1,42 @@
 export default function Skills() {
-    return(
-        <section id="skills" className="h-screen bg-pink-200">
-            my section skills
-        </section>
-    )
+  return (
+    <section id="skills" className="h-screen">
+      <div className="w-full h-screen  flex justify-center flex-col">
+        <div className=" w-full h-1/3 flex justify-center items-center gap-48">
+          <div className="bg-grass-1 h-[95%] aspect-square rounded-full flex items-center justify-center p-6 hover:shadow-lg hover:shadow-grass-9 relative group">
+            <img src="/img/reactJs.png" alt="react logo" />
+            <span className="absolute top-0 -right-16 bg-grass-9 p-2 rounded-md rounded-bl-none opacity-0 group-hover:opacity-100">
+              ReactJs
+            </span>
+          </div>
+          <div className="bg-grass-1 h-[95%] aspect-square rounded-full flex items-center justify-center p-6 hover:shadow-lg hover:shadow-grass-9 relative group">
+            <img src="/img/tailwind.png" alt="tailwind logo" />
+            <span className="absolute top-0 -right-24 bg-grass-9 p-2 rounded-md rounded-bl-none opacity-0 group-hover:opacity-100">
+              TailwindCss
+            </span>
+          </div>
+          <div className="bg-grass-1 h-[95%] aspect-square rounded-full flex items-center justify-center p-6 hover:shadow-lg hover:shadow-grass-9 relative group">
+            <img src="/img/gsap.png" alt="gsap logo" className="pe-6" />
+            <span className="absolute top-0 -right-16 bg-grass-9 p-2 rounded-md rounded-bl-none opacity-0 group-hover:opacity-100">
+              GSAP
+            </span>
+          </div>
+        </div>
+        <div className=" w-full h-1/3 flex justify-center items-center gap-48">
+          <div className="bg-grass-1 h-[95%] aspect-square rounded-full flex items-center justify-center p-6 hover:shadow-lg hover:shadow-grass-9 relative group">
+            <img src="/img/mysql.png" alt="database logo" />
+            <span className="absolute top-0 -right-16 bg-grass-9 p-2 rounded-md rounded-bl-none opacity-0 group-hover:opacity-100">
+              MySql
+            </span>
+          </div>
+          <div className="bg-grass-1 h-[95%] aspect-square rounded-full flex items-center justify-center p-6 hover:shadow-lg hover:shadow-grass-9 relative group">
+            <img src="/img/github.png" alt="github logo" />
+            <span className="absolute top-0 -right-16 bg-grass-9 p-2 rounded-md rounded-bl-none opacity-0 group-hover:opacity-100">
+              GitHub
+            </span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
